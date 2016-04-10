7d2fee04a89a283453b9889b45a094dcb73aa611
Joakim Adrup <adrup94@live.se>
2016-04-10 11:06:23 +0200

    Basic Setup
    Innehåller grundfiler så node.js fungerar.
    model.js app.js och index.html är justerade för att passa vårt projekt.

---
+++ app/js/model.js
@@ -0,0 +1,16 @@
+
+// Back End Modellen
+
+GifTagApp.factory('Model', function ($resource) {
+
+	
+
+  // Angular service needs to return an object that has all the
+  // methods created in it. You can consider that this is instead
+  // of calling var model = new DinnerModel() we did in the previous labs
+  // This is because Angular takes care of creating it when needed.
+  return this;
+
+});
+
+
