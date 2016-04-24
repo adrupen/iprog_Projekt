5d6ac807d219447af3160473383ae16c96d044c6
Jocke <adrup94@live.se>
2016-04-24 15:09:34 +0200

    Super gif page

--- app/js/superGifCrtl.js
+++ app/js/superGifCrtl.js
@@ -1,6 +1,6 @@
 GifTagApp.controller('superGifCrtl', function ($scope,$routeParams,Model) {
 
-	$scope.holidayTag = $routeParams.tag;
+	$scope.tags = new Array();
 
 	$scope.gif = function(id) {
 
@@ -8,11 +8,20 @@ GifTagApp.controller('superGifCrtl', function ($scope,$routeParams,Model) {
 		function(data){
 			if (data.data) {
 				$scope.gif = data.data[0];
+				$scope.divideTags();
 			}
 		},
 		function(data){
 			$scope.status = "There was an error";
 		});
+	}
+	
+	$scope.divideTags = function(){
+		
+		$scope.tags = $scope.gif.slug.replace($routeParams.gif, "");
+		$scope.tags = $scope.tags.substring(0,$scope.tags.length-1);
+		$scope.tags = "#"+$scope.tags.replace(/-/g, " #");
+		
 	}
 
 	$scope.gif($routeParams.gif);
